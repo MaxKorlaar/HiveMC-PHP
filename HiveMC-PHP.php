461c40bf9baf83b619a5dce77b7815915fb2aa38
Max Korlaar <Max@MacBook-Pro-van-Max.local>
2016-02-15 19:49:00 +0100

    Initial commit

---
+++ HiveMC-PHP.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace HiveMCPHP;
+
+    /**
+     * Class HiveMCPHP
+     *
+     * @author    Max Korlaar
+     * @copyright 2016 Max Korlaar
+     * @license   MIT
+     * @credit    to @Plancke for inspiring me in how some things are handled in his HypixelPHP, for example the folder structure for caching files.
+     * @package   HiveMCPHP
+     */
+    class HiveMCPHP
+    {
+
+        function requestJSON($url) {
+            // todo Implement cURL -> json
+        }
+
+    }
+
+    ?>
